f0a0c5f913d5feb1d522319dd4ded1574e2f94fa
pqhuy98 <huypham060398@gmail.com>
2021-03-18 20:23:28 +0200

    update schema

--- database-schema.sql
+++ database-schema.sql
@@ -1,15 +1,26 @@
-# Dump of table media
-# ------------------------------------------------------------
+-- --------------------------------------------------------
+-- Host:                         mysql.cc.puv.fi
+-- Server version:               5.5.62-0+deb8u1 - (Debian)
+-- Server OS:                    debian-linux-gnu
+-- HeidiSQL Version:             10.1.0.5464
+-- --------------------------------------------------------
 
-CREATE TABLE `media` (
-  `id` varchar(255) NOT NULL,
-  `file_name` varchar(255) NOT NULL,
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping structure for table e1601124_media_chest.media
+CREATE TABLE IF NOT EXISTS `media` (
+  `id` varchar(255) NOT NULL DEFAULT '',
+  `file_name` varchar(255) NOT NULL DEFAULT '',
   `has_file` tinyint(4) NOT NULL,
   `text_message` longtext NOT NULL,
   `username` varchar(255) NOT NULL DEFAULT '0',
   `longitude` double DEFAULT NULL,
   `latitude` double DEFAULT NULL,
-  `ip_address` varchar(255) NOT NULL,
+  `ip_address` varchar(255) NOT NULL DEFAULT '',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `created_at` (`created_at`),
@@ -19,23 +30,15 @@ CREATE TABLE `media` (
   CONSTRAINT `point-to-user` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
-
-# Dump of table user
-# ------------------------------------------------------------
-
-CREATE TABLE `user` (
-  `username` varchar(255) NOT NULL,
+-- Data exporting was unselected.
+-- Dumping structure for table e1601124_media_chest.user
+CREATE TABLE IF NOT EXISTS `user` (
+  `username` varchar(255) NOT NULL DEFAULT '',
   `password` varchar(255) NOT NULL,
   PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
-
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+-- Data exporting was unselected.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
